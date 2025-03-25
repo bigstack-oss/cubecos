675c998686ebf13fd17387f16b295eebb262a56d
Jim Lin <jim.lin@bigstack.co>
2025-03-25 23:23:28 +0800

    fix(api): make api only listen to the mgmt address
    Signed-off-by: Jim Lin <jim.lin@bigstack.co>

--- core/modules/config_api.cpp
+++ core/modules/config_api.cpp
@@ -15,6 +15,9 @@
 #define MARKER_API_IDP "/etc/appliance/state/api_idp_done"
 
 static const char API_NAME[] = "cube-cos-api";
+static const char API_CONF_DEF[] = "/etc/cube/api/cube-cos-api.yaml.def";
+static const char API_CONF_IN[] = "/etc/cube/api/cube-cos-api.yaml.in";
+static const char API_CONF[] = "/etc/cube/api/cube-cos-api.yaml";
 
 // external global variables
 CONFIG_GLOBAL_STR_REF(MGMT_ADDR);
@@ -22,6 +25,22 @@ CONFIG_GLOBAL_STR_REF(SHARED_ID);
 
 // rotate daily and enable copytruncate
 static LogRotateConf log_conf(API_NAME, "/var/log/cube-cos-api/*.log", DAILY, 128, 0, true);
+
+static bool
+WriteApiConf(const char* myip)
+{
+    if (HexSystemF(0, "sed -e \"s/@MGMT_ADDR@/%s/\" %s > %s", myip, API_CONF_DEF, API_CONF_IN) != 0) {
+        HexLogError("failed to update %s", API_CONF_IN);
+        return false;
+    }
+
+    if (HexSystemF(0, "cp -f %s %s", API_CONF_IN, API_CONF) != 0) {
+        HexLogError("failed to update %s", API_CONF);
+        return false;
+    }
+
+    return true;
+}
 
 static bool
 CommitCheck(bool modified, int dryLevel)
@@ -47,6 +66,11 @@ Commit(bool modified, int dryLevel)
     if (access(MARKER_API_IDP, F_OK) != 0) {
         HexUtilSystemF(0, 0, HEX_SDK " api_idp_config %s", sharedId.c_str());
         HexSystemF(0, "touch %s", MARKER_API_IDP);
+    }
+
+    std::string myip = G(MGMT_ADDR);
+    if (!WriteApiConf(myip.c_str())) {
+        return false;
     }
 
     // enable cube-cos-api
