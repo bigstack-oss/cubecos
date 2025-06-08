ef9b72fa41bf66cde648d2579170597c8d587e3e
Jim Lin <jim.lin@bigstack.co>
2025-06-08 14:47:44 +0800

    chore(release): v3.0.0-rc4
    Signed-off-by: Jim Lin <jim.lin@bigstack.co>

--- core/skyline/skyline.mk
+++ core/skyline/skyline.mk
@@ -12,10 +12,27 @@ ROOTFS_PIP_NC += gunicorn
 rootfs_install::
 	$(Q)chroot $(ROOTDIR) mkdir -p $(SKYLINE_CONF_DIR) $(SKYLINE_POLICY_DIR) $(SKYLINE_LOG_DIR)
 
+# for RC builds
+heavyfs_install::
+	$(Q)chroot $(ROOTDIR) mkdir -p $(SKYLINE_CONF_DIR) $(SKYLINE_POLICY_DIR) $(SKYLINE_LOG_DIR)
+
 # note: ROOTFS_PIP_DL_FROM is not used since we clone source from master branch instead of targeted openstack branch (stable/yoga)
 # skyline-apiserver installation
 rootfs_install::
-	$(Q)for i in {1..10} ; do timeout 30 git clone -b v3.0.0-rc3 --depth 1 https://github.com/bigstack-oss/skyline-apiserver.git $(ROOTDIR)/skyline-apiserver && break ; done
+	$(Q)chroot $(ROOTDIR) pip3 uninstall -y skyline-apiserver
+	$(Q)chroot $(ROOTDIR) pip3 cache remove skyline-apiserver
+	$(Q)for i in {1..10} ; do timeout 30 git clone -b v3.0.0-rc4 --depth 1 https://github.com/bigstack-oss/skyline-apiserver.git $(ROOTDIR)/skyline-apiserver && break ; done
+	$(Q)chroot $(ROOTDIR) sh -c "cd /skyline-apiserver && pip3 install -r requirements.txt && python3 setup.py install"
+	$(Q)cp ${ROOTDIR}/skyline-apiserver/etc/gunicorn.py ${ROOTDIR}/etc/skyline/gunicorn.py
+	$(Q)$(INSTALL_DATA) $(ROOTDIR) $(COREDIR)/skyline/skyline-apiserver.service ./lib/systemd/system
+	$(Q)$(INSTALL_DATA) -f $(ROOTDIR) $(COREDIR)/skyline/skyline.yaml.in .$(SKYLINE_CONF_DIR)/skyline.yaml.in
+	$(Q)rm -rf $(ROOTDIR)/skyline-apiserver
+
+# for RC builds
+heavyfs_install::
+	$(Q)chroot $(ROOTDIR) pip3 uninstall -y skyline-apiserver
+	$(Q)chroot $(ROOTDIR) pip3 cache remove skyline-apiserver
+	$(Q)for i in {1..10} ; do timeout 30 git clone -b v3.0.0-rc4 --depth 1 https://github.com/bigstack-oss/skyline-apiserver.git $(ROOTDIR)/skyline-apiserver && break ; done
 	$(Q)chroot $(ROOTDIR) sh -c "cd /skyline-apiserver && pip3 install -r requirements.txt && python3 setup.py install"
 	$(Q)cp ${ROOTDIR}/skyline-apiserver/etc/gunicorn.py ${ROOTDIR}/etc/skyline/gunicorn.py
 	$(Q)$(INSTALL_DATA) $(ROOTDIR) $(COREDIR)/skyline/skyline-apiserver.service ./lib/systemd/system
@@ -26,7 +43,20 @@ rootfs_install::
 rootfs_install::
 	$(Q)chroot $(ROOTDIR) pip3 uninstall -y skyline-console
 	$(Q)chroot $(ROOTDIR) pip3 cache remove skyline-console
-	$(Q)for i in {1..10} ; do timeout 30 git clone -b v3.0.0-rc3 --depth 1 https://github.com/bigstack-oss/skyline-console.git $(ROOTDIR)/skyline-console && break ; done
+	$(Q)for i in {1..10} ; do timeout 30 git clone -b v3.0.0-rc4 --depth 1 https://github.com/bigstack-oss/skyline-console.git $(ROOTDIR)/skyline-console && break ; done
+	$(Q)# enable nvm
+	$(Q)sed -i 's/^#//g' $$BASH_ENV
+	$(Q)nvm use $(SKYLINE_NODE_VERSION) $(QEND) && make -C $(ROOTDIR)/skyline-console package
+	$(Q)# disable nvm
+	$(Q)sed -i '/^#/! s/^/#/' $$BASH_ENV
+	$(Q)chroot $(ROOTDIR) sh -c "cd /skyline-console && pip3 install dist/skyline_console-*.whl"
+	$(Q)rm -rf $(ROOTDIR)/skyline-console
+
+# for RC builds
+heavyfs_install::
+	$(Q)chroot $(ROOTDIR) pip3 uninstall -y skyline-console
+	$(Q)chroot $(ROOTDIR) pip3 cache remove skyline-console
+	$(Q)for i in {1..10} ; do timeout 30 git clone -b v3.0.0-rc4 --depth 1 https://github.com/bigstack-oss/skyline-console.git $(ROOTDIR)/skyline-console && break ; done
 	$(Q)# enable nvm
 	$(Q)sed -i 's/^#//g' $$BASH_ENV
 	$(Q)nvm use $(SKYLINE_NODE_VERSION) $(QEND) && make -C $(ROOTDIR)/skyline-console package
