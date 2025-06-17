9a2fd5ad6a56a3424477a6424283276381635b15
Jim Lin <jim.lin@bigstack.co>
2025-06-17 15:49:01 +0800

    chore(bump): bump version to v3.1.0
    Signed-off-by: Jim Lin <jim.lin@bigstack.co>

--- core/skyline/skyline.mk
+++ core/skyline/skyline.mk
@@ -21,7 +21,7 @@ heavyfs_install::
 rootfs_install::
 	$(Q)chroot $(ROOTDIR) pip3 uninstall -y skyline-apiserver
 	$(Q)chroot $(ROOTDIR) pip3 cache remove skyline-apiserver
-	$(Q)for i in {1..10} ; do timeout 30 git clone -b v3.0.0-rc4 --depth 1 https://github.com/bigstack-oss/skyline-apiserver.git $(ROOTDIR)/skyline-apiserver && break ; done
+	$(Q)for i in {1..10} ; do timeout 30 git clone --depth 1 https://github.com/bigstack-oss/skyline-apiserver.git $(ROOTDIR)/skyline-apiserver && break ; done
 	$(Q)chroot $(ROOTDIR) sh -c "cd /skyline-apiserver && pip3 install -r requirements.txt && python3 setup.py install"
 	$(Q)cp ${ROOTDIR}/skyline-apiserver/etc/gunicorn.py ${ROOTDIR}/etc/skyline/gunicorn.py
 	$(Q)$(INSTALL_DATA) $(ROOTDIR) $(COREDIR)/skyline/skyline-apiserver.service ./lib/systemd/system
@@ -43,7 +43,7 @@ heavyfs_install::
 rootfs_install::
 	$(Q)chroot $(ROOTDIR) pip3 uninstall -y skyline-console
 	$(Q)chroot $(ROOTDIR) pip3 cache remove skyline-console
-	$(Q)for i in {1..10} ; do timeout 30 git clone -b v3.0.0-rc4 --depth 1 https://github.com/bigstack-oss/skyline-console.git $(ROOTDIR)/skyline-console && break ; done
+	$(Q)for i in {1..10} ; do timeout 30 git clone --depth 1 https://github.com/bigstack-oss/skyline-console.git $(ROOTDIR)/skyline-console && break ; done
 	$(Q)# enable nvm
 	$(Q)sed -i 's/^#//g' $$BASH_ENV
 	$(Q)nvm use $(SKYLINE_NODE_VERSION) $(QEND) && make -C $(ROOTDIR)/skyline-console package
