297d3f663ab536c50dc88aa4cfcf667a763211fa
bjergsen zhu <bjergsen.zhu@bigstack.co>
2025-12-01 15:05:17 +0800

    fix(appctl): fix the incorrect file preparation path

--- core/appctl/appctl.mk
+++ core/appctl/appctl.mk
@@ -13,14 +13,14 @@ heavyfs_install::
 
 # appctl installation
 APPCTL_RPM = $(TOP_BLDDIR)/core/appctl/appctl.rpm
+APPCTL_PLUGINS_RPM = $(TOP_BLDDIR)/core/appctl/plugins
 
 rootfs_install::
 	$(Q)cp -f $(APPCTL_RPM) $(ROOTDIR)/tmp/
 	$(Q)chroot $(ROOTDIR) dnf install -y /tmp/appctl.rpm
 	$(Q)rm -rf /tmp/appctl.rpm
-	$(Q)cp -f $(COREDIR)/appctl/cube-cos-app-framework.yaml.in $(ROOTDIR)/etc/cube/appctl/
 	$(Q)mkdir -p $(ROOTDIR)/opt/appfw
-	$(Q)cp -r $(COREDIR)/cube-cos-app-framework/{plugins} $(ROOTDIR)/opt/appfw/
+	$(Q)cp -r $(APPCTL_PLUGINS_RPM) $(ROOTDIR)/opt/appfw/
 
 # for RC builds
 heavyfs_install::
@@ -28,4 +28,5 @@ heavyfs_install::
 	$(Q)chroot $(ROOTDIR) rpm -e cube-cos-appctl
 	$(Q)chroot $(ROOTDIR) rpm -i /tmp/appctl.rpm
 	$(Q)rm -rf /tmp/appctl.rpm
-	$(Q)cp -f $(COREDIR)/appctl/cube-cos-app-framework.yaml.in $(ROOTDIR)/etc/cube/appctl/
+	$(Q)mkdir -p $(ROOTDIR)/opt/appfw
+	$(Q)cp -r $(APPCTL_PLUGINS_RPM) $(ROOTDIR)/opt/appfw/
