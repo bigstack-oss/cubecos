6784fdb97b6f32cd4a84bfec2b8fc4fe72212e92
arashi.li <arashi.li@bigstack.co>
2025-12-31 14:54:41 +0800

    feat(prometheus): build  promtool by ourself to solve security issue

--- core/prometheus/prometheus.mk
+++ core/prometheus/prometheus.mk
@@ -6,6 +6,7 @@ ROOTFS_DNF += prometheus2
 PROMETHEUS_REPO = $(shell cp $(COREDIR)/prometheus/prometheus.repo $(ROOTDIR)/etc/yum.repos.d/ ; echo "prometheus")
 
 rootfs_install::
+	$(Q)cp -f $(TOP_BLDDIR)/core/prometheus/promtool $(ROOTDIR)/usr/bin/promtool
 	$(Q)chroot $(ROOTDIR) mkdir -p /var/log/prometheus
 	$(Q)chroot $(ROOTDIR) chown prometheus:prometheus /var/log/prometheus
 	$(Q)chroot $(ROOTDIR) systemctl disable prometheus
