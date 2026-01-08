24490dee3b12040bd221d7b245b7da544520afdd
Jim Lin <jim.lin@bigstack.co>
2026-01-09 01:16:57 +0800

    feat(cinder): add fujitsu eternus dx series fc and iscsi as builtin models
    Signed-off-by: Jim Lin <jim.lin@bigstack.co>

--- core/cinder/cinder.mk
+++ core/cinder/cinder.mk
@@ -5,6 +5,8 @@ ROOTFS_DNF += iscsi-initiator-utils device-mapper-multipath sshpass
 ROOTFS_DNF_NOARCH += openstack-cinder targetcli python3-keystone
 # support other storage backends
 ROOTFS_PIP += purestorage
+# support Fujitsu Eternus DX
+ROOTFS_DNF_NOARCH += python3-pywbem
 
 CINDER_SRCDIR := $(ROOTDIR)/usr/lib/python3.9/site-packages/cinder
 CINDER_PATCHDIR := $(COREDIR)/cinder/$(OPENSTACK_RELEASE)_patch
@@ -30,10 +32,14 @@ rootfs_install::
 	$(Q)$(INSTALL_DATA) $(ROOTDIR) $(COREDIR)/cinder/builtin_models/dell_emc-sc-storagecenter_fc-SCFCDriver.yaml ./usr/share/cube/cos/cinder/builtin_models
 	$(Q)$(INSTALL_DATA) $(ROOTDIR) $(COREDIR)/cinder/builtin_models/dell_emc-powerstore-driver-PowerStoreDriver.yaml ./usr/share/cube/cos/cinder/builtin_models
 	$(Q)$(INSTALL_DATA) $(ROOTDIR) $(COREDIR)/cinder/builtin_models/nfs-NfsDriver.yaml ./usr/share/cube/cos/cinder/builtin_models
+	$(Q)$(INSTALL_DATA) $(ROOTDIR) $(COREDIR)/cinder/builtin_models/fujitsu-eternus_dx-eternus_dx_fc-FJDXFCDriver.yaml ./usr/share/cube/cos/cinder/builtin_models
+	$(Q)$(INSTALL_DATA) $(ROOTDIR) $(COREDIR)/cinder/builtin_models/fujitsu-eternus_dx-eternus_dx_iscsi-FJDXISCSIDriver.yaml ./usr/share/cube/cos/cinder/builtin_models
 	$(Q)chroot $(ROOTDIR) mkdir -p /etc/multipath/conf.d
 	$(Q)$(INSTALL_DATA) $(ROOTDIR) $(COREDIR)/cinder/builtin_models/multipath/dell_emc-sc-storagecenter_fc-SCFCDriver.conf ./etc/multipath/conf.d
 	$(Q)$(INSTALL_DATA) $(ROOTDIR) $(COREDIR)/cinder/builtin_models/multipath/dell_emc-powerstore-driver-PowerStoreDriver.conf ./etc/multipath/conf.d
 	$(Q)$(INSTALL_DATA) $(ROOTDIR) $(COREDIR)/cinder/builtin_models/multipath/nfs-NfsDriver.conf ./etc/multipath/conf.d
+	$(Q)$(INSTALL_DATA) $(ROOTDIR) $(COREDIR)/cinder/builtin_models/multipath/fujitsu-eternus_dx-eternus_dx_fc-FJDXFCDriver.conf ./etc/multipath/conf.d
+	$(Q)$(INSTALL_DATA) $(ROOTDIR) $(COREDIR)/cinder/builtin_models/multipath/fujitsu-eternus_dx-eternus_dx_iscsi-FJDXISCSIDriver.conf ./etc/multipath/conf.d
 	$(Q)chroot $(ROOTDIR) mkdir -p /etc/cube/cos/cinder
 	$(Q)chroot $(ROOTDIR) mkdir -p /etc/cube/cos/cinder/models
 	$(Q)chroot $(ROOTDIR) mkdir -p /etc/cube/cos/cinder/storage_extra_configs_ownership
