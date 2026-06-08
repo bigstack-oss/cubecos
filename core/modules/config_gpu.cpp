453acd44230f6d2ee4f855bb1b08eb7088b1f0e0
steven-chiu-bigstack <steven.chiu@bigstack.co>
2026-06-08 10:49:51 +0800

    feat: add config_gpu that creates and migrates GPU config files
    Signed-off-by: steven-chiu-bigstack <steven.chiu@bigstack.co>

---
+++ core/modules/config_gpu.cpp
@@ -0,0 +1,32 @@
+// CUBE SDK
+
+#include <unistd.h>
+#include <hex/log.h>
+#include <hex/filesystem.h>
+#include <hex/process_util.h>
+#include <hex/config_module.h>
+#include <hex/dryrun.h>
+
+static const char GPU_CONFIG_DIR[]  = "/etc/cube/cos/gpu";
+static const char GPU_CONFIG_FILE[] = "/etc/cube/cos/gpu/config.json";
+
+static bool
+Commit(bool modified, int dryLevel)
+{
+    HEX_DRYRUN_BARRIER(dryLevel, true);
+
+    if (HexMakeDir(GPU_CONFIG_DIR, "root", "root", 0755) != 0) {
+        HexLogError("Failed to create GPU config directory: %s", GPU_CONFIG_DIR);
+        return false;
+    }
+
+    if (access(GPU_CONFIG_FILE, F_OK) != 0) {
+        HexUtilSystemF(0, 0, "echo '[]' > %s", GPU_CONFIG_FILE);
+    }
+
+    return true;
+}
+
+CONFIG_MODULE(gpu, 0, 0, 0, 0, Commit);
+
+CONFIG_MIGRATE(gpu, GPU_CONFIG_DIR);
