425df7f742568326ac88517316c07a9d456b38c8
Jim Lin <jim.lin@bigstack.co>
2026-06-23 14:29:30 +0800

    fix(heavyfs): move dns resolv conf copy step into keystone.mk
    Signed-off-by: Jim Lin <jim.lin@bigstack.co>

--- core/keystone/keystone.mk
+++ core/keystone/keystone.mk
@@ -7,8 +7,12 @@ KEYSTONE_CONF_DIR := /etc/keystone
 
 # install keystone
 rootfs_install::
+	$(Q)# enable dns in the rootfs for downloading packages
+	$(Q)cp -f /etc/resolv.conf $(ROOTDIR)/etc/
 	$(Q)chroot $(ROOTDIR) bash -c "source /opt/openstack-antelope/bin/activate && \
 		pip install -c $(NEXT_OPENSTACK_INSTALLED_PIP_CONSTRAINT) keystone==23.0.2"
+	$(Q)# clean up dns configurations after downloading packages
+	$(Q)rm -f $(ROOTDIR)/etc/resolv.conf
 
 # prepare the build directory
 rootfs_install::
