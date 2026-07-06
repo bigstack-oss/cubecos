228d56149b02a794ad16205734cc7015b7d3a020
Jim Lin <jim.lin@bigstack.co>
2026-07-06 16:40:15 +0800

    feat(cinder): upgrade cinder to openstack antelope
    Signed-off-by: Jim Lin <jim.lin@bigstack.co>

--- core/cinder/cinder.mk
+++ core/cinder/cinder.mk
@@ -1,18 +1,100 @@
 # Cube SDK
 # cinder installation
 
-ROOTFS_DNF += iscsi-initiator-utils sshpass
-ROOTFS_DNF_NOARCH += openstack-cinder targetcli python3-keystone
-# support other storage backends
-ROOTFS_PIP += purestorage
-# support Fujitsu Eternus DX
-ROOTFS_DNF_NOARCH += python3-pywbem
+ROOTFS_DNF += qemu-img cryptsetup lvm2 iscsi-initiator-utils device-mapper-multipath nvmetcli sudo sshpass
+ROOTFS_DNF_NOARCH += targetcli
 
 CINDER_SRCDIR := $(ROOTDIR)/usr/lib/python3.9/site-packages/cinder
 CINDER_PATCHDIR := $(COREDIR)/cinder/$(OPENSTACK_RELEASE)_patch
 
 CINDER_CONFDIR := $(ROOTDIR)/etc/cinder
 
+# install cinder inside the python 3.10 virtual environment
+# purestorage: support Pure Storage
+# pywbem: support Fujitsu Eternus DX
+rootfs_install::
+	$(Q)# enable dns in the rootfs for downloading packages
+	$(Q)cp -f /etc/resolv.conf $(ROOTDIR)/etc/
+	$(Q)chroot $(ROOTDIR) bash -c "source /opt/openstack-antelope/bin/activate && \
+		pip install -c $(NEXT_OPENSTACK_INSTALLED_PIP_CONSTRAINT) \
+			cinder==22.3.0 \
+			python-cinderclient \
+			python-keystoneclient \
+			uwsgi \
+			etcd3gw \
+			websocket-client \
+			purestorage \
+			pywbem"
+	$(Q)# clean up dns configurations after downloading packages
+	$(Q)rm -f $(ROOTDIR)/etc/resolv.conf
+
+# prepare the build directory
+rootfs_install::
+	$(Q)chroot $(ROOTDIR) rm -rf /tmp/cinder
+	$(Q)chroot $(ROOTDIR) mkdir -p /tmp/cinder
+
+# generate default configurations using oslo-config-generator
+rootfs_install::
+	$(Q)cp -f $(COREDIR)/cinder/cinder-config-generator.conf $(ROOTDIR)/tmp/cinder/
+	$(Q)cp -f $(COREDIR)/cinder/cinder.conf.sample $(ROOTDIR)/tmp/cinder/
+	$(Q)# copy statutory configuration templates from core directory
+	$(Q)cp -f $(COREDIR)/cinder/api-paste.ini $(ROOTDIR)/tmp/cinder/
+	$(Q)cp -f $(COREDIR)/cinder/rootwrap.conf $(ROOTDIR)/tmp/cinder/
+	$(Q)cp -f $(COREDIR)/cinder/resource_filters.json $(ROOTDIR)/tmp/cinder/
+	$(Q)cp -f $(COREDIR)/cinder/cinder-sudoers $(ROOTDIR)/tmp/cinder/
+	$(Q)cp -f $(COREDIR)/cinder/volume.filters $(ROOTDIR)/tmp/cinder/
+	$(Q)# copy systemd unit file templates
+	$(Q)cp -f $(COREDIR)/cinder/openstack-cinder-api.service $(ROOTDIR)/tmp/cinder/
+	$(Q)cp -f $(COREDIR)/cinder/openstack-cinder-scheduler.service $(ROOTDIR)/tmp/cinder/
+	$(Q)cp -f $(COREDIR)/cinder/openstack-cinder-volume.service $(ROOTDIR)/tmp/cinder/
+	$(Q)cp -f $(COREDIR)/cinder/openstack-cinder-backup.service $(ROOTDIR)/tmp/cinder/
+	$(Q)cp -f $(COREDIR)/cinder/openstack-cinder.logrotate $(ROOTDIR)/tmp/cinder/
+
+# install system directories and production files
+rootfs_install::
+	$(Q)chroot $(ROOTDIR) install -d -m 755 /var/lib/cinder
+	$(Q)chroot $(ROOTDIR) install -d -m 755 /var/lib/cinder/tmp
+	$(Q)chroot $(ROOTDIR) install -d -m 755 /var/log/cinder
+	$(Q)chroot $(ROOTDIR) install -d -m 755 /etc/cinder
+	$(Q)chroot $(ROOTDIR) install -d -m 755 /etc/cinder/volumes
+	$(Q)chroot $(ROOTDIR) install -d -m 755 /etc/cinder/rootwrap.d
+	$(Q)chroot $(ROOTDIR) install -d -m 755 /var/run/cinder
+	$(Q)# install configurations
+	$(Q)chroot $(ROOTDIR) install -p -D -m 640 /tmp/cinder/cinder.conf.sample /etc/cinder/cinder.conf
+	$(Q)chroot $(ROOTDIR) install -p -D -m 640 /tmp/cinder/api-paste.ini /etc/cinder/api-paste.ini
+	$(Q)chroot $(ROOTDIR) install -p -D -m 640 /tmp/cinder/rootwrap.conf /etc/cinder/rootwrap.conf
+	$(Q)chroot $(ROOTDIR) install -p -D -m 640 /tmp/cinder/resource_filters.json /etc/cinder/resource_filters.json
+	$(Q)# install logrotate and security configurations
+	$(Q)chroot $(ROOTDIR) install -p -D -m 644 /tmp/cinder/openstack-cinder.logrotate /etc/logrotate.d/openstack-cinder
+	$(Q)chroot $(ROOTDIR) install -p -D -m 440 /tmp/cinder/cinder-sudoers /etc/sudoers.d/cinder
+	$(Q)# install systemd unit files
+	$(Q)chroot $(ROOTDIR) install -p -D -m 644 /tmp/cinder/openstack-cinder-api.service /usr/lib/systemd/system/openstack-cinder-api.service
+	$(Q)chroot $(ROOTDIR) install -p -D -m 644 /tmp/cinder/openstack-cinder-scheduler.service /usr/lib/systemd/system/openstack-cinder-scheduler.service
+	$(Q)chroot $(ROOTDIR) install -p -D -m 644 /tmp/cinder/openstack-cinder-volume.service /usr/lib/systemd/system/openstack-cinder-volume.service
+	$(Q)chroot $(ROOTDIR) install -p -D -m 644 /tmp/cinder/openstack-cinder-backup.service /usr/lib/systemd/system/openstack-cinder-backup.service
+	$(Q)# install rootwrap filters into system cinder deployment configuration
+	$(Q)chroot $(ROOTDIR) install -p -D -m 644 /tmp/cinder/volume.filters /etc/cinder/rootwrap.d/
+
+# adjust file ownerships and permissions
+rootfs_install::
+	$(Q)chroot $(ROOTDIR) chown root:cinder /etc/cinder/cinder.conf
+	$(Q)chroot $(ROOTDIR) chown root:cinder /etc/cinder/api-paste.ini
+	$(Q)chroot $(ROOTDIR) chown root:cinder /etc/cinder/rootwrap.conf
+	$(Q)chroot $(ROOTDIR) chown root:cinder /etc/cinder/resource_filters.json
+	$(Q)chroot $(ROOTDIR) chown cinder:root /var/log/cinder
+	$(Q)chroot $(ROOTDIR) chmod 0750 /var/log/cinder
+	$(Q)chroot $(ROOTDIR) chown cinder:root /var/run/cinder
+	$(Q)chroot $(ROOTDIR) chmod 0755 /var/run/cinder
+	$(Q)chroot $(ROOTDIR) chown cinder:root /etc/cinder/volumes
+	$(Q)chroot $(ROOTDIR) chmod 0755 /etc/cinder/volumes
+	$(Q)chroot $(ROOTDIR) chown -R cinder:cinder /var/lib/cinder
+	$(Q)chroot $(ROOTDIR) chown -R cinder:cinder /var/lib/cinder/tmp
+
+# clean up the build directory
+rootfs_install::
+	$(Q)chroot $(ROOTDIR) rm -rf /tmp/cinder
+
+# install custom files
 rootfs_install::
 	$(Q)[ -d $(CINDER_PATCHDIR) ] && cp -rf $(CINDER_PATCHDIR)/* $(CINDER_SRCDIR)/ || /bin/true
 
