10fd84274c36f5856d66e04654769d7f98771377
Jim Lin <jim.lin@bigstack.co>
2026-07-06 11:33:42 +0800

    fix(keystone): write the logrotate config for keystone
    Signed-off-by: Jim Lin <jim.lin@bigstack.co>

--- core/modules/config_keystone.cpp
+++ core/modules/config_keystone.cpp
@@ -484,6 +484,7 @@ Commit(bool modified, int dryLevel)
     // 4. keystone relies on httpd proxy and gunicorn, reached via the VIP; on a cold boot
     // the VIP+haproxy+keystone chain can take >1 min, so wait up to 180s
     SystemdCommitService(IsControl(s_eCubeRole), "openstack-keystone");
+    WriteLogRotateConf(log_conf);
     SystemdCommitService(IsControl(s_eCubeRole), "httpd");
     if (HexUtilSystemF(0, 0, HEX_SDK " wait_for_http_endpoint %s 5000 180", sharedId.c_str()) != 0) {
         return false;
