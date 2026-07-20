689b8e16e244b744cc3dd2a4ea8aebc83a9f2755
Jim Lin <jim.lin@bigstack.co>
2026-07-20 19:04:33 +0800

    feat(neutron): make neutron aware of galera cluster to prevent read after write inconsistencies
    Signed-off-by: Jim Lin <jim.lin@bigstack.co>

--- core/modules/config_neutron.cpp
+++ core/modules/config_neutron.cpp
@@ -330,6 +330,7 @@ UpdateDbConn(std::string sharedId, std::string password)
 
         cfg["database"].clear();
         cfg["database"]["connection"] = dbconn;
+        cfg["database"]["mysql_wsrep_sync_wait"] = "1";
     }
 
     return true;
